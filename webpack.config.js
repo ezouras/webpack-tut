5e93ea96c0bc45908c1e9bb1caa3ba402ffcc657
Zouras <ZOURAEX@abbvienet.com>
2021-01-05 17:12:40 -0600

    cashing html template plugins

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const path = require("path");
 //needed so that webpac knows your current directory
 /* this was just for fun. be sure you change the script
 tag file to pint to hello in CODEE if you use this*/
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+//needed to generate a new dynamic html file
 
 /*module.exports = {
   entry: "./src/index.js",
@@ -13,17 +15,20 @@ tag file to pint to hello in CODEE if you use this*/
 */
 module.exports = {
   mode: "development",
-  devtool: false,
+  /*devtool: false,*/
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "main.[chunkhash].js",
     path: path.resolve(__dirname, "dist")
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: "./src/template.html"
+  })],
   module: {
     rules: [{
       test: /\.s[ac]ss$/i,
       use: [
-        // Creates `style` nodes from JS strings
+        // Creates `style` nodes from JS strings. ORDER MATTERS
         "style-loader",
         // Translates CSS into CommonJS
         "css-loader",
